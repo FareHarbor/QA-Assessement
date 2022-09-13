53dff4ad391197fba479d781a9a4315344d8d666
ArCiGo <armandocifuentes_1@hotmail.com>
2022-09-13 14:46:12 -0500

    Add of Suggestions file. Add of cypress-mochawesome-report

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportWidth: 1880,
   viewportHeight: 1080,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: 'https://demo.fareharbor.com',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
-  
 });
