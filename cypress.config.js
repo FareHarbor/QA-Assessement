105426ccdfa1c6e7aaa17bcbd508d36a57217c63
navaneetha <Navaneetha.Selva.Raman.Thiraviam@rabobank.nl>
2022-09-26 18:10:07 +0530

    Added configuration for creating report

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,24 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportWidth: 1880,
   viewportHeight: 1080,
-
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    "reportDir": "cypress/report",
+    "video": true,
+    "charts": true,
+    "code": false,
+    "overwrite": false,
+    "autoOpen": true,
+    "reportPageTitle": "FareHarbor Automation Report",
+    "reportFilename": "[datetime]-[name]-report",
+    "timestamp": "longDate",
+    "trashAssetsBeforeRuns": true
+  },
   e2e: {
+    baseUrl: "https://demo.fareharbor.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
-  
 });
